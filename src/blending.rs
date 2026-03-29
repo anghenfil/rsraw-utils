bbafcbeac736b7e1838a6050e052949a9c015e0c
anghenfil <keanu@doelle.cc>
2026-03-30 01:37:33 +0200

    improved lib structure, introduced a blending module, added more general raw convert function

---
+++ src/blending.rs
@@ -0,0 +1,125 @@
+use rsraw::RawImage;
+use rsraw_sys::ushort;
+use serde::{Deserialize, Serialize};
+use crate::{raw_pixels, raw_pixels_mut, RsRawUtilsError};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BlendingMode{
+    /// Add the pixel values of all images together.
+    Additive,
+    /// Average the pixel values of all images.
+    Average,
+    /// Only use the brightest pixel value of all images.
+    Bright,
+    /// Only use the darkest pixel value of all images.
+    Dark,
+    /// Prefers the pixel values with the highest deviation from the average.
+    PreferChanged
+}
+
+pub fn blend_raw_images(mut raw_imgs: Vec<RawImage>, mode: BlendingMode) -> Result<RawImage, RsRawUtilsError> {
+    if raw_imgs.len() < 2 {
+        return Err(RsRawUtilsError::NotEnoughImages);
+    }
+
+    let mut main_image = raw_imgs.pop().unwrap();
+    main_image.unpack().map_err(|e| RsRawUtilsError::CouldntUnpack(e.to_string()))?;
+
+    let main_raw_pixels = raw_pixels_mut(&mut main_image)?;
+    let mut pixels = vec![];
+
+    for img in raw_imgs.iter_mut() {
+        img.unpack().map_err(|e| RsRawUtilsError::CouldntUnpack(e.to_string()))?;
+        pixels.push(raw_pixels(img)?);
+    }
+
+    blend_pixels(main_raw_pixels, pixels, &mode);
+    update_metadata(&mut main_image, &mode, (raw_imgs.len()+1) as u32);
+    Ok(main_image)
+}
+
+pub fn blend_pixels(main_image_pixels: &mut [ushort], other_pixels: Vec<&[ushort]>, mode: &BlendingMode){
+    for i in 0..main_image_pixels.len() {
+        let pixel = &mut main_image_pixels[i];
+        match mode{
+            BlendingMode::Additive => {
+                for n in 0..other_pixels.len(){
+                    *pixel = pixel.saturating_add(other_pixels[n][i]) as ushort;
+                }
+            }
+            BlendingMode::Average => {
+                let mut sum = 0;
+                for n in 0..other_pixels.len(){
+                    sum += other_pixels[n][i];
+                }
+                *pixel = (sum / other_pixels.len() as ushort) as ushort;
+            }
+            BlendingMode::Bright => {
+                for n in 0..other_pixels.len(){
+                    if other_pixels[n][i] > *pixel {
+                        *pixel = other_pixels[n][i];
+                    }
+                }
+            }
+            BlendingMode::Dark => {
+                for n in 0..other_pixels.len(){
+                    if other_pixels[n][i] < *pixel {
+                        *pixel = other_pixels[n][i];
+                    }
+                }
+            },
+            BlendingMode::PreferChanged => {
+                let mut pixel_sum = pixel.clone();
+                for n in 0..other_pixels.len(){
+                    pixel_sum += other_pixels[n][i];
+                }
+                let pixel_sum = pixel_sum / other_pixels.len() as ushort;
+
+                let mut biggest_deviation = pixel_sum.abs_diff(*pixel);
+                for n in 0..other_pixels.len(){
+                    let deviation = pixel_sum.abs_diff(other_pixels[n][i]);
+                    if deviation > biggest_deviation{
+                        biggest_deviation = deviation;
+                        *pixel = other_pixels[n][i];
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn update_metadata(image: &mut RawImage, mode: &BlendingMode, num_images: u32){
+    let imgdata_mut = image.as_mut();
+    // Normalisierung der WB-Multiplikatoren.
+    let green_avg = (imgdata_mut.color.cam_mul[1] + imgdata_mut.color.cam_mul[3]) / 2.0;
+    if green_avg > 0.0 {
+        for i in 0..4 {
+            imgdata_mut.color.cam_mul[i] /= green_avg;
+        }
+    }
+
+    if let BlendingMode::Additive = mode {
+        imgdata_mut.color.maximum *= num_images;
+        imgdata_mut.color.data_maximum *= num_images;
+        imgdata_mut.color.black *= num_images;
+    }
+
+    imgdata_mut.params.use_camera_wb = 1;
+    imgdata_mut.params.use_camera_matrix = 1;
+    imgdata_mut.params.no_auto_bright = 0;
+    imgdata_mut.params.output_bps = 16;
+
+    imgdata_mut.params.user_mul = imgdata_mut.color.cam_mul;
+
+    // Wir deaktivieren die automatische Anpassung des Schwarzpegels
+    imgdata_mut.params.user_black = imgdata_mut.color.black as i32;
+    imgdata_mut.params.user_cblack[0] = imgdata_mut.color.cblack[0] as i32;
+    imgdata_mut.params.user_cblack[1] = imgdata_mut.color.cblack[1] as i32;
+    imgdata_mut.params.user_cblack[2] = imgdata_mut.color.cblack[2] as i32;
+    imgdata_mut.params.user_cblack[3] = imgdata_mut.color.cblack[3] as i32;
+
+
+    // sRGB Gamma
+    imgdata_mut.params.gamm[0] = 1.0 / 2.4;
+    imgdata_mut.params.gamm[1] = 12.92;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,18 @@
+pub mod blending;
+
+pub use blending::blend_raw_images;
+
 use std::io::Error;
-use std::path::PathBuf;
+use std::path::Path;
+use image::{ImageBuffer, ImageFormat, Rgb};
 use rsraw::RawImage;
 use tiff::TiffError;
 use rsraw_sys::ushort;
-use tiff::encoder::{colortype, TiffEncoder};
-use image::{ImageBuffer, Rgb};
 use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum BlendingMode{
-    /// Add the pixel values of all images together.
-    Additive,
-    /// Average the pixel values of all images.
-    Average,
-    /// Only use the brightest pixel value of all images.
-    Bright,
-    /// Only use the darkest pixel value of all images.
-    Dark,
-    /// Prefers the pixel values with the highest deviation from the average.
-    PreferChanged
-}
+use tiff::encoder::{colortype, TiffEncoder};
 
 #[derive(Debug)]
-pub enum BlendingError{
+pub enum RsRawUtilsError {
     RsRawError(String),
     CouldntUnpack(String),
     CouldntProcess(String),
@@ -32,19 +22,33 @@ pub enum BlendingError{
     InvalidRawBuffer,
 }
 
-impl From<Error> for BlendingError{
+impl std::fmt::Display for RsRawUtilsError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RsRawUtilsError::RsRawError(e) => write!(f, "{}", e),
+            RsRawUtilsError::CouldntUnpack(e) => write!(f, "{}", e),
+            RsRawUtilsError::CouldntProcess(e) => write!(f, "{}", e),
+            RsRawUtilsError::TiffError(e) => write!(f, "{}", e),
+            RsRawUtilsError::IoError(e) => write!(f, "{}", e),
+            RsRawUtilsError::NotEnoughImages => write!(f, "Not enough images to blend"),
+            RsRawUtilsError::InvalidRawBuffer => write!(f, "Invalid raw buffer"),
+        }
+    }
+}
+
+impl From<Error> for RsRawUtilsError {
     fn from(e: Error) -> Self {
-        BlendingError::IoError(e)
+        RsRawUtilsError::IoError(e)
     }
 }
 
-impl From<TiffError> for BlendingError{
+impl From<TiffError> for RsRawUtilsError {
     fn from(e: TiffError) -> Self {
-        BlendingError::TiffError(e)
+        RsRawUtilsError::TiffError(e)
     }
 }
 
-pub fn raw_pixels(image: &RawImage) -> Result<&[ushort], BlendingError>{
+pub fn raw_pixels(image: &RawImage) -> Result<&[ushort], RsRawUtilsError>{
     let data = image.as_ref();
 
     let raw_width = data.sizes.raw_width as u32;
@@ -54,7 +58,7 @@ pub fn raw_pixels(image: &RawImage) -> Result<&[ushort], BlendingError>{
 
     let ptr = data.rawdata.raw_image;
     if ptr.is_null() {
-        return Err(BlendingError::InvalidRawBuffer);
+        return Err(RsRawUtilsError::InvalidRawBuffer);
     }
 
     unsafe {
@@ -62,7 +66,7 @@ pub fn raw_pixels(image: &RawImage) -> Result<&[ushort], BlendingError>{
     }
 }
 
-fn raw_pixels_mut(image: &mut RawImage) -> Result<&mut [ushort], BlendingError> {
+fn raw_pixels_mut(image: &mut RawImage) -> Result<&mut [ushort], RsRawUtilsError> {
     let data = image.as_mut();
 
     let raw_width = data.sizes.raw_width as u32;
@@ -72,7 +76,7 @@ fn raw_pixels_mut(image: &mut RawImage) -> Result<&mut [ushort], BlendingError> 
 
     let ptr = data.rawdata.raw_image;
     if ptr.is_null() {
-        return Err(BlendingError::InvalidRawBuffer);
+        return Err(RsRawUtilsError::InvalidRawBuffer);
     }
 
     unsafe {
@@ -80,169 +84,50 @@ fn raw_pixels_mut(image: &mut RawImage) -> Result<&mut [ushort], BlendingError> 
     }
 }
 
-fn blend_raw_images(mut raw_imgs: Vec<RawImage>, mode: BlendingMode) -> Result<RawImage, BlendingError> {
-    if raw_imgs.len() < 2 {
-        return Err(BlendingError::NotEnoughImages);
-    }
-
-    let mut main_image = raw_imgs.pop().unwrap();
-    main_image.unpack().map_err(|e| BlendingError::CouldntUnpack(e.to_string()))?;
-
-    let main_raw_pixels = raw_pixels_mut(&mut main_image)?;
-    let mut pixels = vec![];
-
-    for img in raw_imgs.iter_mut() {
-        img.unpack().map_err(|e| BlendingError::CouldntUnpack(e.to_string()))?;
-        pixels.push(raw_pixels(img)?);
-    }
-
-    blend_pixels(main_raw_pixels, pixels, &mode);
-    update_metadata(&mut main_image, &mode, (raw_imgs.len()+1) as u32);
-    Ok(main_image)
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum OutputFormat{
+    JPEG,
+    TIFF,
+    PNG,
 }
 
-#[derive(Debug)]
-pub struct BlendingResult {
-    pub tiff: PathBuf,
-    pub preview: Option<PathBuf>,
-}
+pub fn convert_raw(mut raw_image: RawImage, format: OutputFormat, destination: &Path) -> Result<(), RsRawUtilsError>{
+    match format{
+        OutputFormat::TIFF => {
+            let tiff_processed = raw_image.process::<16>().map_err(|e| RsRawUtilsError::CouldntProcess(e.to_string()))?;
+            let mut tiff_file = std::fs::File::create(destination)?;
+            let mut encoder = TiffEncoder::new(&mut tiff_file)?;
+            encoder.new_image::<colortype::RGB16>(tiff_processed.width(), tiff_processed.height())?.write_data(&tiff_processed)?;
+        },
+        OutputFormat::JPEG => {
+            let processed = raw_image.process::<8>().map_err(|e| RsRawUtilsError::CouldntProcess(e.to_string()))?;
 
-pub fn blend_images(raw_imgs: Vec<RawImage>, mode: BlendingMode, generate_preview: bool) -> Result<BlendingResult, BlendingError>{
-    let mut blended_image = blend_raw_images(raw_imgs, mode)?;
+            let img_buffer: ImageBuffer<Rgb<u8>, _> = ImageBuffer::from_raw(processed.width(), processed.height(), processed.to_vec())
+                .ok_or_else(|| RsRawUtilsError::RsRawError("Failed to create image buffer".to_string()))?;
 
-    let tiff_processed = blended_image.process::<16>().map_err(|e|BlendingError::CouldntProcess(e.to_string()))?;
-    let tiff_path = PathBuf::from(format!("{}.tiff", uuid::Uuid::new_v4()));
-    let mut tiff_file = std::fs::File::create(&tiff_path)?;
-    let mut encoder = TiffEncoder::new(&mut tiff_file)?;
-    encoder.new_image::<colortype::RGB16>(tiff_processed.width(), tiff_processed.height())?.write_data(&tiff_processed)?;
+            img_buffer.save_with_format(destination, ImageFormat::Jpeg).map_err(|e| RsRawUtilsError::RsRawError(e.to_string()))?;
+        },
+        OutputFormat::PNG => {
+            let processed = raw_image.process::<8>().map_err(|e| RsRawUtilsError::CouldntProcess(e.to_string()))?;
 
-    let mut preview_path = None;
-    if generate_preview {
-        // For preview, 8-bit is enough
-        let preview_processed = blended_image.process::<8>().map_err(|e|BlendingError::CouldntProcess(e.to_string()))?;
-        let path = PathBuf::from(format!("{}.jpg", uuid::Uuid::new_v4()));
+            let img_buffer: ImageBuffer<Rgb<u8>, _> = ImageBuffer::from_raw(processed.width(), processed.height(), processed.to_vec())
+                .ok_or_else(|| RsRawUtilsError::RsRawError("Failed to create image buffer".to_string()))?;
 
-        let img_buffer: ImageBuffer<Rgb<u8>, _> = ImageBuffer::from_raw(preview_processed.width(), preview_processed.height(), preview_processed.to_vec())
-            .ok_or_else(|| BlendingError::RsRawError("Failed to create image buffer".to_string()))?;
-
-        img_buffer.save(&path).map_err(|e| BlendingError::RsRawError(e.to_string()))?;
-        preview_path = Some(path);
+            img_buffer.save_with_format(destination, ImageFormat::Png).map_err(|e| RsRawUtilsError::RsRawError(e.to_string()))?;
+        },
     }
-
-    Ok(BlendingResult {
-        tiff: tiff_path,
-        preview: preview_path,
-    })
+    Ok(())
 }
 
-pub fn generate_preview_jpg(raw_imgs: Vec<RawImage>, mode: BlendingMode) -> Result<PathBuf, BlendingError> {
-    let mut blended_image = blend_raw_images(raw_imgs, mode)?;
-    
-    // For preview, 8-bit is enough
-    let processed = blended_image.process::<8>().map_err(|e|BlendingError::CouldntProcess(e.to_string()))?;
-
-    let file_path = PathBuf::from(format!("{}.jpg", uuid::Uuid::new_v4()));
-    
-    let img_buffer: ImageBuffer<Rgb<u8>, _> = ImageBuffer::from_raw(processed.width(), processed.height(), processed.to_vec())
-        .ok_or_else(|| BlendingError::RsRawError("Failed to create image buffer".to_string()))?;
-
-    img_buffer.save(&file_path).map_err(|e| BlendingError::RsRawError(e.to_string()))?;
-
-    Ok(file_path)
-}
-
-pub fn blend_pixels(main_image_pixels: &mut [ushort], other_pixels: Vec<&[ushort]>, mode: &BlendingMode){
-    for i in 0..main_image_pixels.len() {
-        let pixel = &mut main_image_pixels[i];
-        match mode{
-            BlendingMode::Additive => {
-                for n in 0..other_pixels.len(){
-                    *pixel = pixel.saturating_add(other_pixels[n][i]) as ushort;
-                }
-            }
-            BlendingMode::Average => {
-                let mut sum = 0;
-                for n in 0..other_pixels.len(){
-                    sum += other_pixels[n][i];
-                }
-                *pixel = (sum / other_pixels.len() as ushort) as ushort;
-            }
-            BlendingMode::Bright => {
-                for n in 0..other_pixels.len(){
-                    if other_pixels[n][i] > *pixel {
-                        *pixel = other_pixels[n][i];
-                    }
-                }
-            }
-            BlendingMode::Dark => {
-                for n in 0..other_pixels.len(){
-                    if other_pixels[n][i] < *pixel {
-                        *pixel = other_pixels[n][i];
-                    }
-                }
-            },
-            BlendingMode::PreferChanged => {
-                let mut pixel_sum = pixel.clone();
-                for n in 0..other_pixels.len(){
-                    pixel_sum += other_pixels[n][i];
-                }
-                let pixel_sum = pixel_sum / other_pixels.len() as ushort;
-
-                let mut biggest_deviation = pixel_sum.abs_diff(*pixel);
-                for n in 0..other_pixels.len(){
-                    let deviation = pixel_sum.abs_diff(other_pixels[n][i]);
-                    if deviation > biggest_deviation{
-                        biggest_deviation = deviation;
-                        *pixel = other_pixels[n][i];
-                    }
-                }
-            }
-        }
-    }
-}
-
-pub fn update_metadata(image: &mut RawImage, mode: &BlendingMode, num_images: u32){
-    let imgdata_mut = image.as_mut();
-    // Normalisierung der WB-Multiplikatoren.
-    let green_avg = (imgdata_mut.color.cam_mul[1] + imgdata_mut.color.cam_mul[3]) / 2.0;
-    if green_avg > 0.0 {
-        for i in 0..4 {
-            imgdata_mut.color.cam_mul[i] /= green_avg;
-        }
-    }
-
-    if let BlendingMode::Additive = mode {
-        imgdata_mut.color.maximum *= num_images;
-        imgdata_mut.color.data_maximum *= num_images;
-        imgdata_mut.color.black *= num_images;
-    }
-
-    imgdata_mut.params.use_camera_wb = 1;
-    imgdata_mut.params.use_camera_matrix = 1;
-    imgdata_mut.params.no_auto_bright = 0;
-    imgdata_mut.params.output_bps = 16;
-
-    imgdata_mut.params.user_mul = imgdata_mut.color.cam_mul;
-
-    // Wir deaktivieren die automatische Anpassung des Schwarzpegels
-    imgdata_mut.params.user_black = imgdata_mut.color.black as i32;
-    imgdata_mut.params.user_cblack[0] = imgdata_mut.color.cblack[0] as i32;
-    imgdata_mut.params.user_cblack[1] = imgdata_mut.color.cblack[1] as i32;
-    imgdata_mut.params.user_cblack[2] = imgdata_mut.color.cblack[2] as i32;
-    imgdata_mut.params.user_cblack[3] = imgdata_mut.color.cblack[3] as i32;
-
-
-    // sRGB Gamma
-    imgdata_mut.params.gamm[0] = 1.0 / 2.4;
-    imgdata_mut.params.gamm[1] = 12.92;
-}
 
 #[cfg(test)]
 mod tests {
     use std::fs::File;
     use std::io::Read;
     use rsraw::RawImage;
-    use super::*;
+    use crate::blending::{BlendingMode, blend_raw_images};
+    use crate::{convert_raw, OutputFormat};
+    use std::path::Path;
 
     #[test]
     fn load_raw() {
@@ -261,10 +146,11 @@ mod tests {
         raw3.read_to_end(&mut buf3).unwrap();
         let rawfile3 = RawImage::open(&buf3);
 
-        let res = blend_images(vec![rawfile1.unwrap(), rawfile2.unwrap(), rawfile3.unwrap()], BlendingMode::Average, false).unwrap();
-        println!("{:?}", res);
-        assert!(res.tiff.exists());
-        assert!(res.preview.is_none());
+        let res = blend_raw_images(vec![rawfile1.unwrap(), rawfile2.unwrap(), rawfile3.unwrap()], BlendingMode::Average).unwrap();
+        let output_path = Path::new("test_output.tiff");
+        convert_raw(res, OutputFormat::TIFF, output_path).unwrap();
+        assert!(output_path.exists());
+        std::fs::remove_file(output_path).unwrap();
     }
 
     #[test]
@@ -279,15 +165,11 @@ mod tests {
         raw2.read_to_end(&mut buf2).unwrap();
         let rawfile2 = RawImage::open(&buf2);
 
-        let res = blend_images(vec![rawfile1.unwrap(), rawfile2.unwrap()], BlendingMode::PreferChanged, true).unwrap();
-        println!("TIFF saved to: {:?}", res.tiff);
-        println!("Preview saved to: {:?}", res.preview);
-        
-        assert!(res.tiff.exists());
-        assert!(res.preview.is_some());
-        let preview_path = res.preview.unwrap();
-        assert!(preview_path.exists());
-        assert_eq!(preview_path.extension().unwrap(), "jpg");
+        let res = blend_raw_images(vec![rawfile1.unwrap(), rawfile2.unwrap()], BlendingMode::PreferChanged).unwrap();
+        let output_path = Path::new("test_output_with_preview.tiff");
+        convert_raw(res, OutputFormat::TIFF, output_path).unwrap();
+        assert!(output_path.exists());
+        std::fs::remove_file(output_path).unwrap();
     }
 
     #[test]
@@ -302,10 +184,10 @@ mod tests {
         raw2.read_to_end(&mut buf2).unwrap();
         let rawfile2 = RawImage::open(&buf2);
 
-        let res = generate_preview_jpg(vec![rawfile1.unwrap(), rawfile2.unwrap()], BlendingMode::Average).unwrap();
-        println!("Preview saved to: {:?}", res);
-        assert!(res.exists());
-        assert_eq!(res.extension().unwrap(), "jpg");
-        std::fs::remove_file(res).unwrap();
+        let res = blend_raw_images(vec![rawfile1.unwrap(), rawfile2.unwrap()], BlendingMode::Average).unwrap();
+        let output_path = Path::new("test_preview.jpg");
+        convert_raw(res, OutputFormat::JPEG, output_path).unwrap();
+        assert!(output_path.exists());
+        std::fs::remove_file(output_path).unwrap();
     }
 }
